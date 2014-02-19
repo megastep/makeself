9367397bc1767be44109631172efa562a996e06c
Peter Targett <filmlight@agera.filmlight.ltd.uk>
2014-02-19 16:58:24 +0000

    Preserve newline characters
    Preserve newline characters when echoing license string.

--- makeself-header.sh
+++ makeself-header.sh
@@ -38,7 +38,7 @@ MS_Printf()
 MS_PrintLicense()
 {
   if test x"\$licensetxt" != x; then
-    echo \$licensetxt
+    echo "\$licensetxt"
     while true
     do
       MS_Printf "Please type y to accept, n otherwise: "
