f2cd799aecfa8d8141070b02dcc1be7fb84b8472
Arkadiusz Drabczyk <arkadiusz@drabczyk.org>
2017-06-27 22:23:14 +0200

    Prepend error message in UnTAR() with `...' to make it more readable
    Without this:
    
    Uncompressing repoExtraction failed.
    
    Now:
    
    Uncompressing repo ... Extraction failed.

--- makeself-header.sh
+++ makeself-header.sh
@@ -218,7 +218,7 @@ MS_Check()
 UnTAR()
 {
     if test x"\$quiet" = xn; then
-		tar \$1 "$UNTAR_EXTRA" -vf - 2>&1 || { echo Extraction failed. > /dev/tty; kill -15 \$$; }
+		tar \$1 "$UNTAR_EXTRA" -vf - 2>&1 || { echo " ... Extraction failed." > /dev/tty; kill -15 \$$; }
     else
 
 		tar \$1 "$UNTAR_EXTRA" -f - 2>&1 || { echo Extraction failed. > /dev/tty; kill -15 \$$; }
