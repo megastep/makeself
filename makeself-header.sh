6728dec40db52a2d7e718935e99d1efb7832116c
rbcrwd <randy.burton@crowdstrike.com>
2017-11-01 20:29:05 -0600

    Target docfix (#120)
    * Return to tar args that work for Solaris
    
    This should still work with the added UNTAR_EXTRA feature, but returns
    to the known-working tar invocation for rickety old Solaris.
    
    I took the liberty of dropping an extra empty line and the quotes around
    UNTAR_EXTRA so as to preempt any multi-arg problems.
    
    * Make --target documentation more clear
    
    This clarifies the runtime --target documentation so users may clearly
    understand that this is to be a temporary directory (e.g., don't use
    --target=/home). In doing so, we also allude to what's probably the
    right answer for most users, setting TMPDIR instead.
    
    * Further attempt at clarifying

--- makeself-header.sh
+++ makeself-header.sh
@@ -152,8 +152,8 @@ MS_Help()
   --nox11               Do not spawn an xterm
   --nochown             Do not give the extracted files to the current user
   --nodiskspace         Do not check for available disk space
-  --target dir          Extract directly to a target directory
-                        directory path can be either absolute or relative
+  --target dir          Extract directly to a target directory (absolute or relative)
+                        This directory may undergo recursive chown (see --nochown).
   --tar arg1 [arg2 ...] Access the contents of the archive through the tar command
   --                    Following arguments will be passed to the embedded script
 EOH
