c0281dce95335f71edd17b620b89ff8f7d44ecbc
Ing. Mirko Serra <mserra@tecnoaccion.com.ar>
2019-02-15 18:46:26 -0300

    Typo. Corrected description of --nochown and --chown

--- makeself-header.sh
+++ makeself-header.sh
@@ -162,8 +162,8 @@ MS_Help()
                         the embedded script
   --noprogress          Do not show the progress during the decompression
   --nox11               Do not spawn an xterm
-  --nochown             Do not give the extracted files to the current user
-  --chown               Give the extracted files to the current user
+  --nochown             Do not give the target folder to the current user
+  --chown               Give the target folder to the current user recursively
   --nodiskspace         Do not check for available disk space
   --target dir          Extract directly to a target directory (absolute or relative)
                         This directory may undergo recursive chown (see --nochown).
