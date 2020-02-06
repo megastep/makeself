23f49fa4d4c98204376fbdfdb8d9c0a8b1a3bf10
Michal Hagara <michal.hagara@gmail.com>
2020-02-06 23:35:49 +0100

    Printing content of license file (#197) (#198)
    Printing path to the file instead of content of the file was introduced
    in 56c742bd90ebcb22feaff4d78ae4e62e72ffc7ad
    
    Reverting parsing of the license file to the way it was before that
    commit and escaping argument with quotes to address the original issue

--- makeself.sh
+++ makeself.sh
@@ -327,7 +327,7 @@ do
 	;;
     --license)
         # We need to escape all characters having a special meaning in double quotes
-        LICENSE="$(echo "$2" | sed 's/./\\&/g' | xargs)"
+        LICENSE=$(sed 's/\\/\\\\/g; s/"/\\\"/g; s/`/\\\`/g; s/\$/\\\$/g' "$2")
         if ! shift 2; then MS_Usage; exit 1; fi
 	;;
     --follow)
