6e94bdf66ca19d926270216fb3a7a74fbb7e99b9
Stephane Peter <megastep@megastep.org>
2021-03-17 02:38:33 -0700

    Run tests explicitly through bash

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Run every available test
+# Run every available test - Bash needed
 cd test
 for test in *test;
 do
-	./$test || { echo "*** ERROR: Test '$test' failed!"; exit 1; }
+	bash $test || { echo "*** ERROR: Test '$test' failed!"; exit 1; }
 done
