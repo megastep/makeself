d2ec3e9d11335e621e07676d87b78ce7b280c45f
Sagi Haas <sagi.haas@axonius.com>
2021-04-26 13:32:40 +0300

    No check dir + redirect to stderr

--- makeself-header.sh
+++ makeself-header.sh
@@ -200,10 +200,6 @@ MS_Verify_Sig()
     test -x "\$MKTEMP_PATH" || MKTEMP_PATH=\`exec <&- 2>&-; which mktemp || command -v mktemp || type mktemp\`
     skip_lines=\`expr \$(cat \$1 | wc -l) - \$skip + 1 | tr -d " "\`
     temp_dir=\`mktemp -d -t XXXXX\`
-    if [ ! -d $temp_dir ];then
-        echo "Could not create temp dir"
-        exit 2
-    fi
     echo \$SIGNATURE | base64 -d > \$temp_dir/tmp_sig.gpg
     gpg_result=\`tail -n \$skip_lines \$1 | $GPG_PATH --verify \$temp_dir/tmp_sig.gpg - 2>&1\`
     rm -rf \$temp_dir
@@ -211,11 +207,11 @@ MS_Verify_Sig()
         if [ "\$(echo \$gpg_result | grep -c \$sig_key)" -eq "1" ];then
             echo "Signature is good"
         else
-            echo "Signature key does not match"
+            echo "Signature key does not match" >&2
             exit 2
         fi
     else
-        echo "Signature is bad"
+        echo "Signature is bad" >&2
         exit 2
     fi
 }
