159120e689bae176570d1497c1347c43e507facc
Stephane Peter <megastep@megastep.org>
2023-03-11 20:16:41 -0800

    Replace another instance of realpath; add quiet test on platforms

--- test/run-makeself.sh
+++ test/run-makeself.sh
@@ -12,6 +12,10 @@ uname -a
 
 cd "$THISDIR"
 
+# Try a quiet run
+sh "../build-ubuntu/makeself-$VERSION.run" --quiet --target ./tmp/makeself-ubuntu
+
+# Regular runs
 sh "../build-ubuntu/makeself-$VERSION.run" --target ./tmp/makeself-ubuntu
 sh "../build-alpine/makeself-$VERSION.run" --target ./tmp/makeself-alpine
 sh "../build-windows/makeself-$VERSION.run" --target ./tmp/makeself-windows
